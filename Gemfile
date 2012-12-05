69b0d65a705380df413f6c35c2fa73b9ae9187e2
NatashaMurashev <nmurashev@gmail.com>
2012-12-04 20:42:41 -0800

    chargin with stripe now works!

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
+
 gem 'sinatra', '1.1.0'
 gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
--- web.rb
+++ web.rb
@@ -1,22 +1,29 @@
 require 'sinatra'
+require 'stripe'
+
+set :publishable_key, ENV['PUBLISHABLE_KEY']
+set :secret_key, ENV['SECRET_KEY']
+
+Stripe.api_key = settings.secret_key
 
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
 post '/charge' do
-  # set your secret key: remember to change this to your live secret key in production
-  # see your keys here https://manage.stripe.com/account
-  Stripe.api_key = "sk_test_2XxS0Cu8cQ9hDEJUVzEp8j3Z"
+  # Amount in cents
+  @amount = 995
 
-  # get the credit card details submitted by the form
-  token = params[:stripeToken]
-
-  # create the charge on Stripe's servers - this will charge the user's card
-  charge = Stripe::Charge.create(
-    :amount => 995, # amount in cents, again
-    :currency => "usd",
-    :card => token,
-    :description => "payinguser@example.com"
+  Stripe::Charge.create(
+    :amount      => @amount,
+    :card        => params[:stripeToken],
+    :description => 'Sinatra Charge',
+    :currency    => 'usd'
   )
+  
+  error Stripe::CardError do
+    env['sinatra.error'].message
+  end
+
+  File.read(File.join('public', 'index.html'))
 end
