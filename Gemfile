22ba0fd311eb1cfb9b3cde81da9bf19dc1875dc3
NatashaMurashev <nmurashev@gmail.com>
2012-12-04 20:23:24 -0800

    added stripe

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source :rubygems
 gem 'sinatra', '1.1.0'
+gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
--- web.rb
+++ web.rb
@@ -3,3 +3,20 @@ require 'sinatra'
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
+
+post '/charge' do
+  # set your secret key: remember to change this to your live secret key in production
+  # see your keys here https://manage.stripe.com/account
+  Stripe.api_key = "sk_test_2XxS0Cu8cQ9hDEJUVzEp8j3Z"
+
+  # get the credit card details submitted by the form
+  token = params[:stripeToken]
+
+  # create the charge on Stripe's servers - this will charge the user's card
+  charge = Stripe::Charge.create(
+    :amount => 995, # amount in cents, again
+    :currency => "usd",
+    :card => token,
+    :description => "payinguser@example.com"
+  )
+end
